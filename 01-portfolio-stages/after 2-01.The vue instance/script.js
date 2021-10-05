e44dca859f72213e679b480527fac0a09f3dce9a
KroLik99 <isabelle.ebach@gmail.com>
2021-10-05 14:59:08 +0200

    Added a key using the v-bind directive (:)

--- 01-portfolio-stages/after 2-01.The vue instance/script.js
+++ 01-portfolio-stages/after 2-01.The vue instance/script.js
@@ -1,3 +1,33 @@
+let posts= [
+        {
+            id: 1,
+            title: "Why I learned vue",
+            body: "I'm baby chambray street art thundercats occupy four loko\n" +
+            "church-key disrupt. Shaman neutra bushwick chicharrones, tousled\n" +
+            "air plant lomo williamsburg. Listicle aesthetic whatever prism,\n" +
+            "ennui glossier asymmetrical scenester austin intelligentsia\n" +
+            "cronut raw denim umami mumblecore. Lo-fi meh austin, selfies\n" +
+            "hell of tacos 90's vinyl banh mi tbh bicycle rights mumblecore\n" +
+            "tumeric."
+        },
+        {
+            id: 2,
+            title: "Using the Vue CDN",
+            body: "I'm baby chambray street art thundercats occupy four loko\n" +
+            "church-key disrupt. Shaman neutra bushwick chicharrones, tousled\n" +
+            "air plant lomo williamsburg. Listicle aesthetic whatever prism,\n" +
+            "ennui glossier asymmetrical scenester austin intelligentsia\n" +
+            "cronut raw denim umami mumblecore. Lo-fi meh austin, selfies\n" +
+            "hell of tacos 90's vinyl banh mi tbh bicycle rights mumblecore\n" +
+            "tumeric."
+        },
+        {
+            id: 2,
+            title: "Using the Vue CDN",
+            body: "I'm baby chambray street art thundercats occupy four loko."
+        }
+        ]
+
 Vue.createApp({
         data() {
             return {
@@ -10,41 +40,7 @@ Vue.createApp({
 Vue.createApp({
     data(){
         return{
-            posts: [
-                {
-                    id: 1,
-                    title: "Why I learned vue",
-                    body: "I'm baby chambray street art thundercats occupy four loko\n" +
-                        "church-key disrupt. Shaman neutra bushwick chicharrones, tousled\n" +
-                        "air plant lomo williamsburg. Listicle aesthetic whatever prism,\n" +
-                        "ennui glossier asymmetrical scenester austin intelligentsia\n" +
-                        "cronut raw denim umami mumblecore. Lo-fi meh austin, selfies\n" +
-                        "hell of tacos 90's vinyl banh mi tbh bicycle rights mumblecore\n" +
-                        "tumeric."
-                },
-                {
-                    id: 2,
-                    title: "Usind the Vue CDN",
-                    body: "I'm baby chambray street art thundercats occupy four loko\n" +
-                        "church-key disrupt. Shaman neutra bushwick chicharrones, tousled\n" +
-                        "air plant lomo williamsburg. Listicle aesthetic whatever prism,\n" +
-                        "ennui glossier asymmetrical scenester austin intelligentsia\n" +
-                        "cronut raw denim umami mumblecore. Lo-fi meh austin, selfies\n" +
-                        "hell of tacos 90's vinyl banh mi tbh bicycle rights mumblecore\n" +
-                        "tumeric."
-                },
-                {
-                    id: 3,
-                    title: "How I mastered Vue",
-                    body: "I'm baby chambray street art thundercats occupy four loko\n" +
-                        "church-key disrupt. Shaman neutra bushwick chicharrones, tousled\n" +
-                        "air plant lomo williamsburg. Listicle aesthetic whatever prism,\n" +
-                        "ennui glossier asymmetrical scenester austin intelligentsia\n" +
-                        "cronut raw denim umami mumblecore. Lo-fi meh austin, selfies\n" +
-                        "hell of tacos 90's vinyl banh mi tbh bicycle rights mumblecore\n" +
-                        "tumeric."
-                }
-            ],
+            posts: posts,
         };
     },
 }).mount("#blog");
